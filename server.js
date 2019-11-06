cc04531f1afda4215e1e723ea7e6c2022909a33d
Danny Kim <dudkny@gmail.com>
2019-11-06 15:02:00 -0600

    Editing depedencies

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Add routes, both API and view
+// Define API routes here
 app.use(routes);
 
 // Connect to the Mongo DB
