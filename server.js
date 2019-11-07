b5739c499854ffb5ea86376ba7c6ef45c23177c0
Danny Kim <dudkny@gmail.com>
2019-11-07 04:19:44 -0600

    Incorporating Socket.io

--- server.js
+++ server.js
@@ -5,6 +5,48 @@ const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+//-----------------//
+// Socket.io setup //
+//-----------------//
+// const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(PORT, () =>
+  console.log(`🌎  ==> Socket.io initialized API Server to now listen on PORT ${PORT}!`)
+);
+
+// WARNING: app.listen(80) will NOT work here!
+
+app.get('/', function (req, res) {
+  console.log(__dirname);
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function (socket) {
+  // socket.emit('signon', 'Welcome to Bookgle');
+
+  // socket.broadcast.emit('user just connected');
+
+  // socket.on('connect', function (data) {
+  //   console.log(data);
+  // });
+
+  // socket.on('my other event', function (data) {
+  //   console.log(data);
+  // });
+
+  // socket.emit('news', { hello: 'world1' });
+
+
+});
+
+//------------------------//
+// End of Socket.io setup //
+//------------------------//
+
+
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +58,8 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
+
+
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
@@ -25,7 +69,7 @@ mongoose.connect(
   }
 );
 
-// Start the API server
-app.listen(PORT, () =>
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
-);
+// Express starts the API server
+// app.listen(PORT, () =>
+//   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+// );
